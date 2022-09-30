981e5c6599ea03efcfb06c9361d01a3e659b98f1
Calum <okthisisannoyingstopit@gmail.com>
2022-09-30 01:23:27 -0300

    Added more JS and finishing touches

--- public/Scripts/app.js
+++ public/Scripts/app.js
@@ -1,4 +1,11 @@
-//Tests to see if the script section is initialized
+/* File: App.css
+Name: Calum Bashow
+Student ID# 301218933
+Date: 28/09/2002
+*/
+
+//Main Javascript
+
 (function(){
     function Start()
     {
@@ -9,15 +16,20 @@
 
 })();
 
-function readInfo (){
+//Takes and reads the info of the user
+function InfoSubmit (){
 let firstName =   document.getElementById("inputFirstName").value;
 let lastName = document.getElementById("inputFirstName").value;
 let phoneNumber = document.getElementById("inputEmail").value;
 let email = document.getElementById("inputPhone").value;
 let message = document.getElementById("inputMessage").value;
-console.log(firstName + " " + lastName )
 
+//Gives user a chance to deny submitting and returning to home page
+let confirmationText = "Would you like to submit and return to the home page ?"
+
+if (confirm(confirmationText) == true)
+{
+    window.location.href = "/home";
 }
-document.getElementById("submitButton").onclick = readInfo;
-
-
+}
+document.getElementById("submitButton").onclick = InfoSubmit;
