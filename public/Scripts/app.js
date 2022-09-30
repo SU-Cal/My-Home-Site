94f252bca7e8c235d35ea0962c0edfaf04478acf
Calum <okthisisannoyingstopit@gmail.com>
2022-09-29 21:00:15 -0300

    Added Javascrpt content for reading boxes

--- public/Scripts/app.js
+++ public/Scripts/app.js
@@ -1,3 +1,4 @@
+//Tests to see if the script section is initialized
 (function(){
     function Start()
     {
@@ -7,3 +8,16 @@
     window.addEventListener("load", Start);
 
 })();
+
+function readInfo (){
+let firstName =   document.getElementById("inputFirstName").value;
+let lastName = document.getElementById("inputFirstName").value;
+let phoneNumber = document.getElementById("inputEmail").value;
+let email = document.getElementById("inputPhone").value;
+let message = document.getElementById("inputMessage").value;
+console.log(firstName + " " + lastName )
+
+}
+document.getElementById("submitButton").onclick = readInfo;
+
+
